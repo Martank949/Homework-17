8cf8ea6e62cd476f61d791c1e3959bd1b9df2378
Gargantua <martinzb949@gmail.com>
2021-03-03 12:29:21 -0800

    updated api.js

--- routes/api.js
+++ routes/api.js
@@ -1,44 +1,60 @@
-const API = {
-    async getLastWorkout() {
-        let res;
-        try {
-            res = await fetch("/api/workouts");
-        } catch (err) {
-            console.log(err);
-        }
-        const json = await res.json();
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
-        return json[json.length - 1];
-    },
-    async addExercise(data) {
-        const id = location.search.split("=")[1];
+router.post("/api/workouts", (req, res) => {
+	Workout.create({})
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
-        const res = await fetch("/api/workouts/" + id, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-        });
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+	Workout.findByIdAndUpdate(
+		params.id,
+		{ $push: { exercises: body } },
+		{ new: true, runValidators: true },
+	)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
-        const json = await res.json();
+router.get("/api/workouts", (req, res) => {
+	Workout.find()
+		.then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
-        return json;
-    },
-    async createWorkout(data = {}) {
-        const res = await fetch("/api/workouts", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: { "Content-Type": "application/json" },
-        });
+router.get("/api/workouts/range", (req, res) => {
+	Workout.find({})
+		.limit(7)
+		.then((dbWorkouts) => {
+			console.log(dbWorkouts);
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
-        const json = await res.json();
+router.delete("/api/workouts", ({ body }, res) => {
+	Workout.findByIdAndDelete(body.id)
+		.then(() => {
+			res.json(true);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
-        return json;
-    },
-
-    async getWorkoutsInRange() {
-        const res = await fetch(`/api/workouts/range`);
-        const json = await res.json();
-
-        return json;
-    },
-};
+module.exports = router;
